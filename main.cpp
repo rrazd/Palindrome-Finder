0eeaea233b74f44244c654d5b2e1996fa478d0e4
Rashna Razdan <rashnarazdan@gmail.com>
2011-07-07 23:33:09 -0700

    Format changes

--- main.cpp
+++ main.cpp
@@ -7,35 +7,27 @@ using namespace std;
 
 int main () { 
  
-	int digits_specified = 0;
-	
+	int digits_specified = 0;	
 	cout << "\n\n(_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}\n\n"; 
-
 	cout << "Please enter the number of digits for operands: ";
 	cin>>digits_specified;
-	cout << "\n";
-	
+	cout << "\n";	
 	FindPalindrome trial1(digits_specified);
-
 	//find palindrome and record the time it took to do this
 	struct timeval start, end;
-    long mtime, seconds, useconds;
-    
-    gettimeofday(&start, NULL);
+        long mtime, seconds, useconds;
+        gettimeofday(&start, NULL);
 	//start actual calculation for palindrome
 	trial1.Product_Generator();
-	gettimeofday(&end, NULL);
-    
+	gettimeofday(&end, NULL);  
 	seconds  = end.tv_sec  - start.tv_sec;
-    useconds = end.tv_usec - start.tv_usec;
-    mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;
+        useconds = end.tv_usec - start.tv_usec;
+        mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;
 	cout << "\n";
-    printf("Elapsed time: %ld milliseconds\n", mtime);
+        printf("Elapsed time: %ld milliseconds\n", mtime);
  	cout << "\n";
-	cout<<endl<< "Largest palimdromic number: "<< trial1.Greatest_Palindrome_Found()<<endl; 
-
+	cout<<endl<< "Largest palindromic number: "<< trial1.Greatest_Palindrome_Found()<<endl; 
 	cout << "\n\n(_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}\n\n"; 
-
 	cout << "\n";
-    return 0;
+        return 0;
 }
